4564ddb863cf9702a36d0fe54abd173836372859
ysk1031 <yusukeaono.1031@gmail.com>
2016-01-04 21:58:03 +0900

    Update setting

--- Brewfile
+++ Brewfile
@@ -1,38 +1,20 @@
-update
-
-upgrade
-
-install zsh
-install git
-install vim
-install rbenv
-install ruby-build
-install mysql
-install redis
-install tmux
-install gh
-install tig
-install tree
-install readline
-install imagemagick
-install node
-install wget
-install jq
-install nkf
-install go
-
-# for nokogiri
-# install libxml2
-# install libxslt
-# install libiconv
-
-# install mecab
-# install mecab-ipadic
-
-tap peco/peco
-install peco
-
-tap motemen/ghq
-install ghq
-
-cleanup
+brew 'zsh'
+brew 'git'
+brew 'rbenv'
+brew 'rbenv-default-gems'
+brew 'ruby-build'
+brew 'mysql'
+brew 'redis'
+brew 'node'
+brew 'hub'
+brew 'tig'
+brew 'wget'
+brew 'go'
+brew 'peco'
+brew 'libxml2'
+brew 'libxslt'
+brew 'imagemagick'
+brew 'mecab'
+brew 'mecab-ipadic'
+brew 'ngrok'
+brew 'gibo'
--- my.zsh
+++ my.zsh
@@ -1,29 +1,5 @@
-autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
-add-zsh-hook chpwd chpwd_recent_dirs
-zstyle ':chpwd:*' recent-dirs-max 500
-zstyle ':chpwd:*' recent-dirs-default yes
-zstyle ':completion:*' recent-dirs-insert both
-
 setopt hist_ignore_all_dups
 
-# gh
-eval "$(gh alias -s)"
-
-
-alias v='vim'
-alias a='atom'
-alias r='rails'
-alias ao='RAILS_ENV=aono'
-alias gsl='git stash list'
-alias gd='git diff'
-alias gb='git branch -a'
-alias gg='git grep'
-alias gf='git fetch --prune'
-alias vg='vagrant'
-alias -g B='`git branch -a | peco | sed -e "s/^\*[ ]*//g"`'
-
-
-# peco
 function peco_select_history() {
   local tac
   if which tac > /dev/null; then
@@ -38,16 +14,6 @@ function peco_select_history() {
 zle -N peco_select_history
 bindkey '^r' peco_select_history
 
-function peco_cdr() {
-  local selected_dir=$(cdr -l | awk '{ print $2 }' | peco)
-  if [ -n "$selected_dir" ]; then
-    BUFFER="cd ${selected_dir}"
-    zle accept-line
-  fi
-  zle clear-screen
-}
-zle -N peco_cdr
-bindkey '^xb' peco_cdr
 
 function peco-src () {
   local selected_dir=$(ghq list -p | peco --query "$LBUFFER")
@@ -60,11 +26,13 @@ function peco-src () {
 zle -N peco-src
 bindkey '^]' peco-src
 
-function cdgem() {
-  local gem_name=$(bundle list | sed -e 's/^ *\* *//g' | peco | cut -d \  -f 1)
-  if [ -n "$gem_name" ]; then
-    local gem_dir=$(bundle show ${gem_name})
-    echo "cd to ${gem_dir}"
-    cd ${gem_dir}
-  fi
-}
+
+alias g='git'
+alias gst='git status'
+alias gd='git diff'
+alias gg='git grep'
+alias gb='git branch'
+alias gf='git fetch --prune'
+alias a='atom'
+
+alias -g B='`git branch -a | peco --prompt "GIT BRANCH>" | head -n 1 | sed -e "s/^\*\s*//g"`'
